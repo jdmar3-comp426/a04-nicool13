56aad72b344f23e0c01b2b12ccf92142315bc9ee
Nicole Sherman <nicool@live.unc.edu>
2021-12-02 11:58:00 -0500

    fixed delete

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.patch("/app/update/user/:id", (req, res) => {
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 
-app.get("/app/delete/user/:id", (req, res) => {	
+app.delete("/app/delete/user/:id", (req, res) => {	
 	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?");
 	const info = stmt.run(req.params.id)
 	res.status(200).json({"message":""+info.changes+" record deleted: ID "+req.params.id+" (200)"});
